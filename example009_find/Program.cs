2e8d1b838afb4807c091f64f49eafabc6ce789ff
Darya <koterushina@bk.ru>
2022-08-10 21:57:50 +0300

    find

---
+++ example009_find/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array={11,12,13,14,15,666,777,333,14};
+
+int n= array.Length;
+int find=14;
+
+int index=0;
+
+while (index<n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
